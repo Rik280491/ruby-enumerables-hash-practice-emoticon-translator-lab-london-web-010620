c1876b328f1610feb3a51d0a79f26d0bb47bc992
Rikesh Balakrishnan <rik280491@googlemail.com>
2020-01-04 19:48:42 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -1,18 +1,3 @@
 # require modules here
 require "yaml"
 
-def load_library
-  # code goes here
-  emoticons.yml
-  
-end
-end 
-
-
-def get_japanese_emoticon
-  # code goes here
-end
-
-def get_english_meaning
-  # code goes here
-end
